b6e5c8eee70711d97667ddb548bd52380da99fa8
YashodharaSandeepani <it22327062@my.sliit.lk>
2024-05-10 00:19:18 +0530

    Add new files in second time

--- app/src/main/java/com/example/notessqlite/Note.kt
+++ app/src/main/java/com/example/notessqlite/Note.kt
@@ -1,3 +1,6 @@
 package com.example.notessqlite
 
-data class Note()
+import android.app.assist.AssistContent
+import android.icu.text.CaseMap.Title
+
+data class Note(val id: Int, val title: String, val content: String)
