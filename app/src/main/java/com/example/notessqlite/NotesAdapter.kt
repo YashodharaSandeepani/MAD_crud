94d6e857d003134de5b2ab4816ce1ed69c88bf6a
YashodharaSandeepani <it22327062@my.sliit.lk>
2024-05-10 14:06:36 +0530

    Delete data in SQLite database

--- app/src/main/java/com/example/notessqlite/NotesAdapter.kt
+++ app/src/main/java/com/example/notessqlite/NotesAdapter.kt
@@ -7,15 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class NotesAdapter(private var notes: List<Note>, context: Context) :
     RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
-    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
-        val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
-        val updateButton: ImageView = itemView.findViewById(R.id.updateButton)
-    }
+
+    private val db: NotesDatabaseHelper = NotesDatabaseHelper(context)
+        class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
+            val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
+            val updateButton: ImageView = itemView.findViewById(R.id.updateButton)
+            val deleteButton: ImageView = itemView.findViewById(R.id.deleteButton)
+        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false)
@@ -35,6 +39,13 @@ class NotesAdapter(private var notes: List<Note>, context: Context) :
             }
             holder.itemView.context.startActivity(intent)
         }
+
+        holder.deleteButton.setOnClickListener {
+            db.deleteNote(note.id)
+            refreshData(db.getAllNotes())
+            Toast.makeText(holder.itemView.context, "Note Deleted", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     fun refreshData(newNotes: List<Note>){
